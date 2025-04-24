21033593440a24516132ca9368ef4877558cf8cb
AhmadSalman <ahmadsalman.ghafoor@capellinisauro.edu.it>
2025-04-24 07:34:24 +0000

    prima modifica

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        
+        for(int i=0;i<3;i++){
+        System.out.println("Ahmad Salman");
+        }
+        System.out.println("20/08/2006");
+        System.out.println("Forza Squadra");
     }
 }
