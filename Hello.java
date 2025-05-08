cdc3a583c3577244466271e1c6740a12fd182928
AhmadSalman <ahmadsalman.ghafoor@capellinisauro.edu.it>
2025-05-08 07:26:02 +0000

    array dinamico vector

--- Hello.java
+++ Hello.java
@@ -1,5 +1,18 @@
 public class Hello {
     public static void main(String[] args) {
+       System.out.println("Hello, Ghafoor!");
        
+       Vector<Character> lettere = new Vector<>();
+       lettere.add('a');
+       lettere.add('b');
+       lettere.add('c');
+       lettere.add('d');
+       lettere.add('e');
+       lettere.add('f');
+       lettere.add('g');
+       
+       for(Character lettera : lettera) {
+     System.out.println(lettera);   
+       }
     }   
 }
