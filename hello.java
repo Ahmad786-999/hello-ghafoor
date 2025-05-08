800efabcaac3acbb6e8903eea7d5da1e7cfd4096
AhmadSalman <ahmadsalman.ghafoor@capellinisauro.edu.it>
2025-05-08 06:36:27 +0000

    Aggiunta stampa da Array Statico

--- hello.java
+++ hello.java
@@ -1,11 +1,25 @@
 public class hello {
     public static void main(String[] args) {
+        
+
         System.out.println("Hello, World!");
         
         for(int i=0;i<3;i++){
         System.out.println("Ahmad Salman");
         }
         System.out.println("20/08/2006");
+
+
         System.out.println("Forza Spezia");
     }
+    
+    System.out.println("Hello, Ghafoor!");
+    
+        //Array di caratteri con le lettere del nome "Ghafoor"
+        char[] nome = {'g', 'h', 'a', 'f', 'o', 'o', 'r'};
+        
+        //Stampa le lettere dell'array
+        for (char lettera : lettere) {
+            System.out.print(lettera + "");
+        }
 }
