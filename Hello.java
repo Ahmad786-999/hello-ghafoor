c53aa9c904d0674e25ceb671deeeda232a1a70b9
AhmadSalman <ahmadsalman.ghafoor@capellinisauro.edu.it>
2025-05-08 07:18:40 +0000

    risoluzione errore

--- Hello.java
+++ Hello.java
@@ -1,14 +1,6 @@
-import java.util.Vector;
-public class hello {
+public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        
-        for(int i=0;i<3;i++){
-        System.out.println("Ahmad Salman");
-        }
-        System.out.println("20/08/2006");
-        System.out.println("Forza Spezia");
-
+       
         Vector<Character> lettere = new Vector<>();
         lettere.add('g');
         lettere.add('h');
---
+++ nome.java
@@ -0,0 +1,10 @@
+public class nome-con-array {
+    public static void main(String[] args) {
+ System.out.print("Hello Ghafoor")  ;
+ 
+ char[] nome = {'G', 'h', 'a', 'f', 'o', 'o', 'r'};
+ for(char lettere: lettere){
+        System.out.print(lettera + "");
+ }
+}
+}
