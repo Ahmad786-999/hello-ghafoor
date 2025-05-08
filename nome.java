f33e581df15b65701592dd7eb116fbf330755bc3
AhmadSalman <ahmadsalman.ghafoor@capellinisauro.edu.it>
2025-05-08 07:22:22 +0000

    array statico

--- nome.java
+++ nome.java
@@ -1,10 +1,10 @@
-public class Hello {
+public class nome-con-array {
     public static void main(String[] args) {
-        System.out.print("Hello Ghafoor")  ;
+ System.out.print("Hello Ghafoor")  ;
  
  char[] nome = {'G', 'h', 'a', 'f', 'o', 'o', 'r'};
  for(char lettere: lettere){
         System.out.print(lettera + "");
  }
-    }   
+}
 }
