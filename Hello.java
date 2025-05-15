1fb101fffcdedc8516ddf4206596cae01a3e5052
Edo-oss <edoardo.scolaro@capellinisauro.edu.it>
2025-05-15 07:11:52 +0000

    Modifica Scolaro 1

--- Hello.java
+++ Hello.java
@@ -1,18 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
        
-        Vector<Character> lettere = new Vector<>();
-        lettere.add('g');
-        lettere.add('h');
-        lettere.add('a');
-        lettere.add('f');
-        lettere.add('o');
-        lettere.add('o');
-        lettere.add('r');
         
-        for(char lettera : lettere){
-            System.out.println(lettera);
-
+            System.out.println("Scolaro è stato qui");
         }
     }
-}
+
